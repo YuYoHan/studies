25532aa9a2cd14f6fa482540a5259024796a8c69
YuYoHan <zxzz8014@naver.com>
2025-01-22 00:00:08 +0900

    [Add] 페이지 세팅

---
+++ src/main/java/org/example/oauth2/controller/MyController.java
@@ -0,0 +1,12 @@
+package org.example.oauth2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping("/my")
+    public String myPage() {
+        return "my";
+    }
+}
