ee8f1f4af463bd824c99fe873718b697fa75db9a
YuYoHan <zxzz8014@naver.com>
2025-02-27 22:56:26 +0900

    [Study] 디자인패턴 공부

---
+++ Design/study/src/Adapter/ex02/PrintBanner.java
@@ -0,0 +1,18 @@
+package Adapter.ex02;
+
+public class PrintBanner extends Banner implements Print {
+
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    @Override
+    public void printWeak() {
+        showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        showWithAster();
+    }
+}
